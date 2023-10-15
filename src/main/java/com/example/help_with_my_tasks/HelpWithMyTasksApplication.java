82839fc3725d410bdfe72d3f72ae1031bbbd058b
ayushsinha3393 <ayush.sinha3393@gmail.com>
2023-10-15 13:50:08 +0530

    Initialising Project - Connected with DB

---
+++ src/main/java/com/example/help_with_my_tasks/HelpWithMyTasksApplication.java
@@ -0,0 +1,13 @@
+package com.example.help_with_my_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpWithMyTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpWithMyTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/help_with_my_tasks/HelpWithMyTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.help_with_my_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpWithMyTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
