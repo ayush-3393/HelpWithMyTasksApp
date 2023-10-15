4768146f10154934a74c66907e398b5b16820da8
ayushsinha3393 <ayush.sinha3393@gmail.com>
2023-10-15 14:27:12 +0530

    Completed Payment Model

--- src/main/java/com/example/help_with_my_tasks/models/Payment.java
+++ src/main/java/com/example/help_with_my_tasks/models/Payment.java
@@ -1,4 +1,26 @@
 package com.example.help_with_my_tasks.models;
 
-public class Payment {
+import com.example.help_with_my_tasks.models.enums.PaymentStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@Entity
+public class Payment extends BaseModel{
+    private Integer amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    private Date paymentDate;
+
+    @OneToOne
+    private Booking booking;
 }
